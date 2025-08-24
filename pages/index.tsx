e092092392b76920334eeac60d0ce4e9c448ec94
ashkankh <ashkan1@outlook.com>
2025-08-24 12:11:48 +0330

    use api in component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { todosData } from "@/data/todos";
+import { todosType } from "@/types/todos.type";
+import { useEffect, useState } from "react";
+
+
+export default function Index() {
+  const [todos, setTodos] = useState<todosType[]>([])
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("api/todos")
+      const data = await res.json();
+      setTodos(data)
+    }
+    fetchData();
+  },[] )
   return (
     <>
       Home
+      {todos.map((todo) => (<li key={todo.id}>{todo.title}</li>))}
     </>
   );
 }
