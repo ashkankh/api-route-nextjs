0e61b0f5d317d8eb23015656b6d08bbc99204cf3
ashkankh <ashkan1@outlook.com>
2025-08-24 20:18:29 +0330

    add POST method

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -1,10 +1,22 @@
 
 import { todosData } from "@/data/todos";
+import { todosType } from "@/types/todos.type";
 import { NextApiRequest, NextApiResponse } from "next";
 import { title } from "process";
 
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(todosData)
+    if (req.method === "GET") {
+        res.status(200).json(todosData)
+    } else if (req.method === "POST") {
+        const { todo } = req.body
+
+        const newTodo: todosType = {
+            id: todosData.length + 1,
+            title: todo,
+        };
+        res.status(201).json({ message: "success", data: newTodo, })
+    }
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import { todosData } from "@/data/todos";
 import { todosType } from "@/types/todos.type";
+import { title } from "process";
 import { useEffect, useState } from "react";
 
 
 export default function Index() {
   const [todos, setTodos] = useState<todosType[]>([])
+  const [todo, setTodo] = useState<string>("")
+
+  const addHandler = async () => {
+    const res = await fetch("api/todos", {
+      method: "POST",
+      body: JSON.stringify({ todo }),
+      headers: { "Content-Type": "application/json" }
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch("api/todos")
@@ -12,11 +25,15 @@ export default function Index() {
       setTodos(data)
     }
     fetchData();
-  },[] )
+  }, [])
   return (
     <>
       Home
       {todos.map((todo) => (<li key={todo.id}>{todo.title}</li>))}
+      <div className="flex flex-row items-center">
+        <input className="flex p-1 m-1  rounded-lg border-2 border-blue-400" onChange={(e) => setTodo(e.target.value)} placeholder="Enter Todo"></input>
+        <button className="flex justify-center items-center px-10 h-9  bg-blue-500 rounded-md text-white" onClick={addHandler}>add</button>
+      </div>
     </>
   );
 }
